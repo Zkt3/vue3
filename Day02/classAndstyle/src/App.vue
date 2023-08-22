a37a7f03ddbabacd1270bd232d5f2e3301cc6d58
Zkt3 <2313726595@qq.com>
2023-08-22 15:18:07 +0800

    class && style

---
+++ Day02/classAndstyle/src/App.vue
@@ -0,0 +1,45 @@
+<script>
+  export default {
+    data(){
+      return{
+        isActive:true,
+        error:null,
+        activeClass:"active",
+        activeColor:'red',
+        fontSize:'50px',
+        bgColor:'pink'
+      }
+    },
+    computed:{
+      classobj:function (){
+        return{
+          active:this.isActive &&  !this.error
+        }
+      }
+
+    }
+  }
+</script>
+
+<template>
+  <div>
+<!--第一种，放置字符串-->
+<!--    <p class="active">hello</p>-->
+<!--&lt;!&ndash;第二种,放置对象&ndash;&gt;-->
+<!--    <p :class="{active:isActive}">hello1</p>-->
+<!--    <button @click="isActive=!isActive">改变active</button>-->
+<!--    <p :class="classobj">hello2</p>-->
+<!--    <p :class="[activeClass]">hello3</p>-->
+
+    <p style="color: red">hello</p>
+    <p :style="{color:activeColor,fontSize:fontSize,'background-color':bgColor}">hello1</p>
+  </div>
+
+</template>
+
+<style scoped>
+  .active{
+    font-size: 50px;
+    color: blue;
+  }
+</style>
--- vue3笔记.md
+++ vue3笔记.md
@@ -200,3 +200,62 @@ user:{
 }
 ```
 
+
+
+### class与style绑定
+
+#### 	class
+
+##### 		class类名的对象使用方式(经常和computed联用)
+
+​	
+
+<script>
+  export default {
+    data(){
+      return{
+        isActive:true,
+        error:null
+      }
+    },
+    computed:{
+      classobj:function (){
+        return{
+          active:this.isActive &&  !this.error
+        }
+      }
+​    }
+  }
+</script>
+
+<template>
+  <div>
+<!--第一种，放置字符串-->
+    <p class="active">hello</p>
+<!--第二种,放置对象-->
+    <p :class="{active:isActive}">hello1</p>
+    <button @click="isActive=!isActive">改变active</button>
+    <p :class="classobj">hello2</p>
+  </div>
+
+</template>
+
+<style scoped>
+  .active{
+    font-size: 50px;
+    color: blue;
+  }
+</style>
+
+
+
+##### class类名的数组使用方式
+
+<p :class="[activeClass]">hello3</p>
+
+
+
+#### style
+
+<p style="color: red">hello</p>
+<p :style="{color:activeColor,fontSize:fontSize,'background-color':bgColor}">hello1</p>
